3263020ca0d2fa09d5b2b5ddd5d4a76031567142
gabchak <andrey.gabchak@gmail.com>
2018-12-24 12:39:28 +0200

    added test of hashMap's solving

--- src/test/java/com/gabchak/TwoSumTest.java
+++ src/test/java/com/gabchak/TwoSumTest.java
@@ -4,8 +4,6 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TwoSumTest {
 
     private TwoSum twoSum;
@@ -16,20 +14,39 @@ class TwoSumTest {
     }
 
     @Test
-    void twoSum() {
+    void twoSumBruteForce() {
         int[] inputArray = {2, 7, 11, 15};
         int[] resultIndexes = {0, 1};
         int target = 9;
 
-        Assert.assertArrayEquals(resultIndexes, twoSum.twoSum(inputArray, target));
+        Assert.assertArrayEquals(resultIndexes, twoSum.twoSumBruteForce(inputArray, target));
     }
 
     @Test
-    void twoSum2() {
+    void twoSumBruteForce2() {
         int[] inputArray = {3, 3};
         int[] resultIndexes = {0, 1};
         int target = 6;
 
-        Assert.assertArrayEquals(resultIndexes, twoSum.twoSum(inputArray, target));
+        Assert.assertArrayEquals(resultIndexes, twoSum.twoSumBruteForce(inputArray, target));
+    }
+
+    @Test
+    void twoSumBruteForce3() {
+        int[] inputArray = {3, 3, 5, 8, 6, 4};
+        int[] resultIndexes = {0, 5};
+        int target = 7;
+
+        int[] actuals = twoSum.twoSumBruteForce(inputArray, target);
+        Assert.assertArrayEquals(resultIndexes, actuals);
+    }
+
+    @Test
+    void twoSumWithHashMap() {
+        int[] inputArray = {2, 7, 11, 15};
+        int[] resultIndexes = {0, 1};
+        int target = 9;
+
+        Assert.assertArrayEquals(resultIndexes, twoSum.twoSumUseHashMap(inputArray, target));
     }
 }
