d713b3a83d58eb973a1233bfa90aa65a3e634670
gabchak <andrey.gabchak@gmail.com>
2018-12-24 12:38:54 +0200

    added solving which use hashMap

--- src/main/java/com/gabchak/TwoSum.java
+++ src/main/java/com/gabchak/TwoSum.java
@@ -1,10 +1,12 @@
 package com.gabchak;
 
-//1
+//1 https://leetcode.com/problems/two-sum/
+
+import java.util.HashMap;
 
 public class TwoSum {
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSumBruteForce(int[] nums, int target) {
         int[] resultIndex = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
@@ -12,9 +14,31 @@ public class TwoSum {
                 if (nums[i] + nums[j] == target) {
                     resultIndex[0] = i;
                     resultIndex[1] = j;
+                    return resultIndex;
                 }
             }
         }
-        return resultIndex;
+        throw new IllegalArgumentException("wrong arguments");
     }
+
+    public int[] twoSumUseHashMap(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (map.containsKey(difference)) {
+                Integer index = map.get(difference);
+                if (index != i) {
+                    return new int[] {i, index};
+                }
+            }
+        }
+        throw new IllegalArgumentException("wrong arguments");
+    }
+
+
 }
